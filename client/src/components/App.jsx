f1e33e25df318e29261ea5b96db0528252d98759
tarrinneal <tarrinneal@gmail.com>
2021-02-23 17:33:40 -0800

    completed set up of react and webpack and bael

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <p>LOADED</p>
+    )
+  }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,24 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          }
+        },
+      },
+    ],
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx']
   }
 };
+
