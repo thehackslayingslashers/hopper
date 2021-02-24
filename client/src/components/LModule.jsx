781e918384e3081cc6105a108958b7ee3fcdb32f
Lerroy Mwaghore <lmwaghore@gmail.com>
2021-02-24 16:23:42 -0600

    Added linter and configured it. Need to find a way to test all files

--- client/src/components/LModule.jsx
+++ client/src/components/LModule.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 class LModule extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      something: []
-    }
+    };
   }
 
   render() {
-    return (<div class='LModule'>Lerroy's Module goes here</div>)
+    return (<div className="LModule">Lerroys Module goes here</div>);
   }
 }
 
