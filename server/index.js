c41f00b75a6ff09cb63df4a838ea3369b43e687e
tarrinneal <tarrinneal@gmail.com>
2021-02-23 16:54:53 -0800

    added basic server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('../public'));
+
+app.get('/', (req, res) => {
+  res.send('nice');
+});
+
+app.listen(port, () => {
+  console.log(`Server is live and happenin on ${port}`)
+})
