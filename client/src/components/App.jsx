6df1927d09f0a96c3ca8903253cb395d83eb728f
tarrinneal <tarrinneal@gmail.com>
2021-02-27 13:33:22 -0800

    added api call to get data about current item

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
 import OverviewMod from './OverviewMod/OverviewMod';
 import RelatedItemsAndComparison from './RelatedItemsAndComparison/RelatedItemsAndComparison';
@@ -8,7 +9,29 @@ import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      currentItemId: 17762,
+    };
+    this.getInfoAboutCurrentItem = this.getInfoAboutCurrentItem.bind(this);
+  }
+
+  getInfoAboutCurrentItem() {
+    let productId = this.state.currentItemId;
+
+    axios
+      .get(`/product/${productId}`)
+      .then((data) => {
+        this.setState({
+          currentItemInfo: data.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.getInfoAboutCurrentItem();
   }
 
   render() {
--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-console */
 const express = require('express');
+const axios = require('axios');
+
+const dotenv = require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +12,22 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('nice');
+});
+
+app.get('/product/:id', (req, res) => {
+  let id = req.params.id;
+  let options = {
+    headers: {
+      Authorization: process.env.GITHUB_API_KEY
+    }
+  }
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${id}`, options)
+    .then((response) => {
+      res.send(response.data)
+    })
+    .catch((error) => {
+      res.status(500).send(error)
+    })
 });
 
 app.listen(port, () => {
