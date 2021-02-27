192ac3dc5991962259bb1415896ce13256fc47c4
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-02-26 18:30:46 -0800

    Added RelatedItemsAndComparison folder and relevant components

---
+++ client/src/components/RelatedItemsAndComparison/OutfitCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class OutfitCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render () {
+    return (
+      <div>
+        This is an outfit card.
+      </div>
+    )
+  }
+}
+
+export default OutfitCard;
---
+++ client/src/components/RelatedItemsAndComparison/RelatedProductCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class RelatedProductCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render () {
+    return (
+      <div>
+        This is a Related Product card.
+      </div>
+    )
+  }
+}
+
+export default RelatedProductCard;
