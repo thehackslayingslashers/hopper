6a3a4ffa232012f651e59551ac72bc5796f87dae
tarrinneal <tarrinneal@gmail.com>
2021-03-01 00:20:42 -0800

    working on adding checkmark, bug with selecting items now though

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -54,9 +54,14 @@ class App extends React.Component {
   handleStyleSelection(e) {
     let index = Number(e.target.attributes.index.nodeValue);
     if (this.state.selectedStyleIndex !== index)
-      this.setState({
-        selectedStyleIndex: index,
-      });
+      this.setState(
+        {
+          selectedStyleIndex: index,
+        },
+        () => {
+          console.log(this.state.selectedStyleIndex);
+        }
+      );
   }
 
   componentDidMount() {
