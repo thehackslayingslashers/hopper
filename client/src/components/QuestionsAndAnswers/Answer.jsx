8f2e9d7f1347ae42e653c1dedf9607b7e2b604ba
Gareth Rogers <garethdavisrogers@gmail.com>
2021-03-04 13:31:28 -0600

    shows proper questions now

--- client/src/components/QuestionsAndAnswers/Answer.jsx
+++ client/src/components/QuestionsAndAnswers/Answer.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 const Answer = ({ answerId, answers }) => {
   var parsedAnswerId = parseInt(answerId);
   return (
-    <div className="a-for-answer-and-answer-text">
-      <div>A: </div>
-      <div className="answer-text">{answers[parsedAnswerId].body}</div>
+    <div className="answer-item">
+      <div className="a-for-answer-and-answer-text">
+        <div>A: </div>
+        <div className="answer-text">{answers[parsedAnswerId].body}</div>
+      </div>
+      <div>username and date</div>
+      <div className="upvote-answer-button">Helpful?</div>
+      <div className="small-divider">|</div>
+      <div className="report-answer-button">Report</div>
     </div>
   );
 };
