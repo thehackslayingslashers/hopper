9bf5650fb55a1fe2f4ca5065019ca49a9cee2dcd
tarrinneal <tarrinneal@gmail.com>
2021-03-06 20:45:06 -0800

    added axios call on front end for click tracker

--- client/src/components/withClickTracker.jsx
+++ client/src/components/withClickTracker.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import axios from 'axios';
 
 const withClickTracker = (WrappedComponent) => {
   class WithClickTracker extends React.Component {
@@ -10,6 +12,17 @@ const withClickTracker = (WrappedComponent) => {
 
     onClickAnywhere(e) {
       console.log('Your FBI agent says hi!');
+      console.log(WrappedComponent.name, new Date(), e.target);
+      const data = {
+        widget: WrappedComponent.name,
+        date: new Date(),
+        element: JSON.stringify(e.target),
+      };
+      axios({
+        url: '/interactions',
+        method: 'post',
+        data,
+      });
     }
 
     render() {
