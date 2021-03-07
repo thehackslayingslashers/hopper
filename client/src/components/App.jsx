71608150dbebb3efa28ac0de06258cc7bf57af8d
tarrinneal <tarrinneal@gmail.com>
2021-03-06 19:25:28 -0800

    fixed thumbnail bug

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -14,7 +14,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      currentItemId: 17075,
+      currentItemId: 17072,
       currentItemInfo: {},
       currentItemRatingInfo: {},
       currentItemAverageRating: 0,
