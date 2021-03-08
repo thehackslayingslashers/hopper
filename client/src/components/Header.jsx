ab00ded83ff29698748045892ec0ec91b74be926
tarrinneal <tarrinneal@gmail.com>
2021-03-08 14:38:19 -0800

    search bar is gone?

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,8 +5,7 @@ const Header = ({ searchValue, searchId, handleSearchIdChange }) => (
   <>
     <div id="pageHeader">
       <h1>LOGO</h1>
-      {/* <img
-        src="public/HOPR.png"></img> */}
+      {/* <img src="../public/HOPR.png"></img> */}
       <form
         onSubmit={searchId}
         id="headerSearch"
