4c23a7ff2b69fd623a0bbbdf54e04a51c6c45f0f
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-03-09 14:26:01 -0800

    outfit carousel working

--- client/src/components/RelatedItemsAndComparison/OutfitList.jsx
+++ client/src/components/RelatedItemsAndComparison/OutfitList.jsx
@@ -10,11 +10,12 @@ class OutfitList extends React.Component {
     this.state = {
       outfitIds: [],
       outfits: [],
-      track: '',
-      slides: [],
+      slidePosition: 0,
     };
     this.handleAddClick = this.handleAddClick.bind(this);
     this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    this.handleLeftClick = this.handleLeftClick.bind(this);
+    this.handleRightClick = this.handleRightClick.bind(this);
   }
 
   componentDidMount() {
@@ -22,6 +23,8 @@ class OutfitList extends React.Component {
     const slides = Array.from(track.children);
     console.log(track)
     console.log(slides)
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    console.log(slideWidth)
     const storedOutfitIds = JSON.parse(window.localStorage.getItem('outfitIds'));
     const storedOutfits = JSON.parse(window.localStorage.getItem('outfits'));
     if (storedOutfits !== null) {
@@ -37,9 +40,10 @@ class OutfitList extends React.Component {
       this.setState({
         outfitIds: storedOutfitIds,
         outfits: storedOutfits,
+      }, () => {
+        console.log(this.state)
       });
     }
-
   }
 
   handleAddClick() {
@@ -77,14 +81,40 @@ class OutfitList extends React.Component {
     }
   }
 
+  handleLeftClick(e) {
+    const { slidePosition } = this.state;
+    const track = document.querySelector('.outfit-slide');
+    if (slidePosition - 264 >= 0) {
+      const newPosition = slidePosition - 264;
+      track.style.transform = 'translateX(-' + newPosition + 'px' + ')';
+      this.setState({
+        slidePosition: newPosition,
+      });
+    }
+  }
+
+  handleRightClick() {
+    const { slidePosition, outfits } = this.state;
+    const track = document.querySelector('.outfit-slide');
+    const numberOfCards = outfits.length + 1;
+    console.log(numberOfCards)
+    if (slidePosition + 264 <= (numberOfCards - 4) * 264) {
+      const newPosition = slidePosition + 264;
+      track.style.transform = 'translateX(-' + newPosition + 'px' + ')';
+      this.setState({
+        slidePosition: newPosition,
+      });
+    }
+  }
+
   render({ outfits } = this.state, { handleCardClick } = this.props) {
     return (
       <div>
         <h2>Your Outfit</h2>
         <div className="carousel-wrapper">
-          <p className="carousel-left-button"><BiLeftArrow /></p>
-          <div className="carousel-track">
-            <div className="carousel-slide">
+          <p className="carousel-left-button" onClick={this.handleLeftClick}><BiLeftArrow /></p>
+          <div className="carousel-track outfit-carousel">
+            <div className="carousel-slide outfit-slide">
               <AddOutfitCard
                 handleAddClick={this.handleAddClick}
               />
@@ -100,7 +130,7 @@ class OutfitList extends React.Component {
               }
             </div>
           </div>
-            <p className="carousel-right-button"><BiRightArrow /></p>
+            <p className="carousel-right-button" onClick={this.handleRightClick}><BiRightArrow /></p>
         </div>
       </div>
     );
