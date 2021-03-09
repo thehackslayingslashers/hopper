7b99319c0538c6134ae16f3abd7c9cf507164813
tarrinneal <tarrinneal@gmail.com>
2021-03-09 15:41:25 -0800

    fixed fade out bug

--- client/src/components/OverviewMod/ImageGallery.jsx
+++ client/src/components/OverviewMod/ImageGallery.jsx
@@ -20,7 +20,7 @@ class ImageGallery extends React.Component {
       thumbnailIndex: 0,
       tx: 0,
       ty: 0,
-      fadeIn: true,
+      fadeIn: false,
     };
     this.handleFullScreen = this.handleFullScreen.bind(this);
     this.handleImageClick = this.handleImageClick.bind(this);
@@ -51,7 +51,7 @@ class ImageGallery extends React.Component {
       selectedImageIndex: index,
       fadeIn: true,
     }, () => {
-      setTimeout(this.unFadeIn, 1000);
+      setTimeout(this.unFadeIn, 500);
     });
   }
 
@@ -106,10 +106,11 @@ class ImageGallery extends React.Component {
       default:
         break;
     }
-    setTimeout(this.unFadeIn, 1000);
+    setTimeout(this.unFadeIn, 500);
   }
 
   unFadeIn() {
+
     this.setState({
       fadeIn: false,
     });
@@ -165,7 +166,7 @@ class ImageGallery extends React.Component {
       }
       return null;
     });
-    setTimeout(this.unFadeIn, 500);
+
     return currentItemStyles[selectedStyleIndex].photos[selectedImageIndex].url ? (
       <div
         id="overviewImageGallery"
