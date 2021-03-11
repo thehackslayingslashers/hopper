1989241146e5d5a7da4bc1d465413889f8e0f9a2
Gareth Rogers <garethdavisrogers@gmail.com>
2021-03-11 17:27:53 -0600

    updated style

--- client/src/components/QuestionsAndAnswers/AnswerList.jsx
+++ client/src/components/QuestionsAndAnswers/AnswerList.jsx
@@ -9,6 +9,7 @@ class AnswerList extends React.Component {
       answersDisplayed: 2,
       showLessAnswersButtonDisplay: { display: 'none' },
       answerDisplay: { display: 'flex' },
+      totalAnswers: 0,
     };
 
     this.handleShowMoreAnswers = this.handleShowMoreAnswers.bind(this);
@@ -16,9 +17,9 @@ class AnswerList extends React.Component {
   }
 
   handleShowMoreAnswers(e) {
-    const { answersDisplayed } = this.state;
+    const { answersDisplayed, totalAnswers } = this.state;
     e.preventDefault();
-    if (answersDisplayed < 50 || answersDisplayed > 2) {
+    if (answersDisplayed < 50 && totalAnswers > 2) {
       // const plusTwoAnswerLength = answersDisplayed + 2;
       this.setState({ answersDisplayed: 50 });
       this.setState({ showLessAnswersButtonDisplay: { display: 'flex' } });
@@ -29,18 +30,21 @@ class AnswerList extends React.Component {
     const { answersDisplayed } = this.state;
     if (answersDisplayed > 2) {
       // const minusTwoAnswerLength = answersDisplayed - 2;
-      this.setState({ answersDisplayed: 2 });
-      if (answersDisplayed < 3) {
-        this.setState({ showLessAnswersButtonDisplay: { display: 'none' } });
-      }
+      this.setState({ answersDisplayed: 2, showLessAnswersButtonDisplay: { display: 'none' } });
     }
+  }
+
+  componentDidMount() {
+    const { answers } = this.props;
+    const keyArray = Object.keys(answers);
+    this.setState({ totalAnswers: keyArray.length });
   }
 
   render() {
     const { answers, currentQuestion } = this.props;
-    const { answersDisplayed, showLessAnswersButtonDisplay } = this.state;
+    const { answersDisplayed, showLessAnswersButtonDisplay, totalAnswers } = this.state;
     const keyArray = Object.keys(answers);
-    var slicedKeyArray = keyArray.slice(0, answersDisplayed);
+    const slicedKeyArray = keyArray.slice(0, answersDisplayed);
     return (
       <div id="answers-container">
         {slicedKeyArray.map((answerId, index) => (
